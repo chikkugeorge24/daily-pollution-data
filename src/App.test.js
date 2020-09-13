77d415d1e11120fdc2c868f3ba5d959a9a8f06e2
chikkugeorge24 <59620586+chikkugeorge24@users.noreply.github.com>
2020-09-14 00:52:31 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("redirects to DataCheck page", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <App/>
+    </Router>
+  );
+  expect(history.location.pathname).toBe("/");
+
 });
