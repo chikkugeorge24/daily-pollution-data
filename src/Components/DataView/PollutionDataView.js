e3423784c23143d675a132283bcf4acf4969f07f
chikkugeorge24 <59620586+chikkugeorge24@users.noreply.github.com>
2020-09-13 23:02:34 +0530

    Create PollutionDataView.js

---
+++ src/Components/DataView/PollutionDataView.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Alert, Result, Button, Empty } from 'antd';
+
+import PollutionGraph from '../Graph/PollutionGrpah';
+import useSpinner from '../Spinner/useSpinner';
+
+/** PollutionDataView loads the PollutionGraph */
+const PollutionDataView = (props) => {
+      const [pollutionData, setPollutionData] = useState([]);
+      const [pollutionDataLength, setPollutionDataLength] = useState();
+      const [responseStatus, setResponseStatus] = useState(200);
+      const [spinner, showSpinner, hideSpinner] = useSpinner();
+      const [isLoaded, setIsLoaded] = useState(true);
+      const { city, date } = props.location;
+      const DashBoardTitle = {
+            fontWeight: "500",
+            border: "inset",
+            backgroundColor: "rgb(204 87 87 / 33%)",
+            fontVariant: "petite-caps",
+            fontSize: "x-large",
+            marginTop: "10px",
+            width: "60vw"
+      };
+      useEffect(() => {
+            const fetchPollutionData = async () => {
+                  showSpinner();
+                  axios.get(`https://api.openaq.org/v1/measurements?limit=25&city=${city}&date_from=${date}&date_to=${date}`)
+                        .then(response => {
+                              return response.data.results;
+                        }).then(results => {
+                              const pollutionDataLength = results.length;
+                              setPollutionDataLength(pollutionDataLength);
+                              if (pollutionDataLength) {
+                                    const groupedLocations = results.reduce((result, current) => {
+                                          result[current.location] = [...result[current.location] || [], current];
+                                          return result;
+                                    }, {});
+                                    const pollutionDataArray = [];
+                                    for (const [location, value] of Object.entries(groupedLocations)) {
+                                          const pollutionDataObj = {};
+                                          value.forEach(element => {
+                                                const { parameter, value } = element;
+                                                pollutionDataObj[parameter] = value;
+                                          });
+                                          const splicedLocation = location.split(',');
+                                          pollutionDataObj.location = splicedLocation[0];
+                                          pollutionDataArray.push(pollutionDataObj);
+                                    }
+                                    setPollutionData(pollutionDataArray);
+                              }
+                              setTimeout(() => hideSpinner(), setIsLoaded(false), 1000);
+                        }).catch(err => {
+                              if (err.response) {
+                                    setResponseStatus(err.response.status);
+                              }
+                              setTimeout(() => hideSpinner(), setIsLoaded(false), 20);
+                        });
+
+            };
+            fetchPollutionData();
+      }, []);
+
+      const goBackToHomePage = () => {
+            props.history.push('./pollution-data-check');
+      };
+
+      const graphTitle = (
+            <Alert
+                  message={`Pollution Data in ${city} On ${date}`}
+                  style={DashBoardTitle} />
+      );
+
+      let dataOutput = spinner;
+      if (!isLoaded) {
+            if (responseStatus === 200) {
+                  if (pollutionDataLength) {
+                        dataOutput = (
+                              <div className="col-8">
+                                    {graphTitle}
+                                    <PollutionGraph
+                                          graphData={pollutionData}/>
+                              </div>
+                        );
+
+                  } else {
+                        dataOutput = (
+                              <div className="col-8">
+                                    {graphTitle}
+                                    <Empty
+                                          image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+                                          imageStyle={{
+                                                height: "50vh",
+                                          }}
+                                          description="Sorry... No data found."
+                                    >
+                                          <Button
+                                                type="primary"
+                                                onClick={goBackToHomePage}
+                                          >Back Home</Button>
+                                    </Empty>
+                              </div>
+                        );
+                  }
+            } else {
+                  dataOutput = (
+                        <div className="col-8">
+                              <Result
+                                    status="404"
+                                    title="400"
+                                    subTitle="Sorry..The page you visited doesn't exist."
+                              />
+                        </div>
+                  );
+            }
+      }
+
+      return (
+            <div className="container-fluid h-100">
+                  <div className="row h-100 justify-content-center">
+                        {dataOutput}
+                  </div>
+            </div>
+
+      );
+};
+export default PollutionDataView;
