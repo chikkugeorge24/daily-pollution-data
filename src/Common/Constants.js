9e446acd1a6c9a5d43d5bdc9048c5386db8033b0
chikkugeorge24 <59620586+chikkugeorge24@users.noreply.github.com>
2020-09-14 09:17:53 +0530

    Update Constants.js

--- src/Common/Constants.js
+++ src/Common/Constants.js
@@ -1,6 +1,8 @@
 const citiesURL =  "https://api.openaq.org/v1/cities?country=IN";
 const  dataURL = "https://api.openaq.org/v1/measurements?limit=25";
+const noDataImgURL = "https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg";
 export {
       citiesURL,
-      dataURL
+      dataURL,
+      noDataImgURL
 }
